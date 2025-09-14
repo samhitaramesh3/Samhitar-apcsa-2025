853a793888db4a67fdb702ba9e30900a0d8c1941
samhitaramesh3 <sr0381@pleasantonusd.net>
2025-09-14 04:46:23 +0000

    working?

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -15,7 +15,7 @@ public class Roomba implements Directions {
 	}
     
 	// declared here so it is visible in all the methods!
-	 private Robot roomba;
+	 private Robot Rom = new Robot(7,6,South,50);
 
 	// You will need to add many variables!!
 
@@ -24,25 +24,80 @@ public class Roomba implements Directions {
 
 		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
         // Make sure it starts at startX and startY location.
-		int totalBeepers = 0; 
-		roomba = new Robot(7,6,East,0);
+
 		World.readWorld(worldName);
 		World.setVisible(true);
-		while (roomba.frontIsClear()){
-			while(roomba.nextToABeeper()){
-				roomba.pickBeeper();
-				totalBeepers++;
-			roomba.move();
-		}
-	}
-}
+		while (true) {
+			while (Rom.nextToABeeper()) {
+        Rom.pickBeeper();
+    }
+	if (Rom.frontIsClear()) {
+        Rom.move();
+    } else {
 
-
-
-    
-
-
-		
+		if (Rom.facingEast()) {
+            Rom.turnLeft();       // turn North
+            if (Rom.frontIsClear()) Rom.move();
+            Rom.turnLeft();       // face West
+            else break;  
+			 } else if (Rom.facingWest()) {
+				
+		Rom.turnLeft();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.move();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.turnLeft();
+		Rom.move();
+		Rom.move();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.moveLeft();
+		Rom.moveLeft();
+		Rom.moveLeft();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
+		Rom.move();
+		Rom.move();
+		Rom.move();
+		Rom.pickBeeper();
 
 		/** This section will have all the logic that takes the Robot to every location
 		 * and cleans up all piles of beepers. Think about ways you can break this
@@ -53,7 +108,7 @@ public class Roomba implements Directions {
 		// what is that and why are we getting it?
 		
 
-		// Need to move this somewhere else.
+		int totalBeepers = 0; // Need to move this somewhere else.
         // This method should return the total number of beepers cleaned up.
 		return totalBeepers;
 	}
