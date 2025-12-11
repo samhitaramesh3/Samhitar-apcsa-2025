38f5a5654a4e73b7ea6fc63c32c5064202abbe9a
samhitaramesh3 <sr0381@pleasantonusd.net>
2025-12-11 23:10:58 +0000

    final

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 
 public class GameOfLife implements Board {
 
-    // Integers: 0 or 1 for alive or dead
+    // Integers, 0 or 1 for alive or dead
     private int[][] board;
     public GameOfLife(int x, int y)
     {
-        // Construct a 2d array of the given x and y size.
+        // Construct a 2d array of the given x and y size
         board = new int[x][y];
         
     }
     //Set values on the board
     public void set(int x, int y, int[][] data) {
+      
         for (int i = 0; i < data.length; i++) {
+           
             for (int j = 0; j < data[0].length; j++) {
                 board[i + x][j + y] = data[i][j];
             }
@@ -53,6 +55,8 @@ public class GameOfLife implements Board {
                     }
                 }
             }
+        }
+        board = newBoard;
         print();
         // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
     }
@@ -60,12 +64,12 @@ public class GameOfLife implements Board {
 
     public int countNeighbors(int x, int y) {
         int count = 0;
-        // count the number of neighbors the cell has
-        // use the get(x,y) method to read any board state you need.
+        // counting
+        // (x,y) method to read any board state you need
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 if (i == 0 && j == 0) {
-                    continue; // Skip the cell itself
+                    continue; // Skip 
                 }
                 count += get(x + i, y + j);
             }
@@ -73,9 +77,8 @@ public class GameOfLife implements Board {
         return count;
     }
 
-    // Get a value from the board with "wrap around"
-    // Locations outside the board will loop back into the board.
-    // Ex: -1 will read board.length-1
+    // Get a value from the board
+    // Locations outside the board will loop back into the board
     public int get(int x, int y) {
         int xLimit = board.length;
         int yLimit= board[0].length;
@@ -98,6 +101,7 @@ public class GameOfLife implements Board {
 
         for (int x = 0; x < board.length; x++) {
             System.out.print("\n" + x%10);
+           
             for (int y=0; y<board[x].length; y++)
             {
                 if (board[x][y] == 1)
